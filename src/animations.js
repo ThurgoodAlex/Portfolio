5faa28ad13a230ad528953be5ba89a71d8fd833d
Alex Thurgood <adthurgood@gmail.com>
2024-08-06 16:15:40 -0600

    Changed up color scheme

--- src/animations.js
+++ src/animations.js
@@ -1,2 +1,3 @@
 //add typewriter effect to biography
 //https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_typewriter
+//https://www.freecodecamp.org/news/reusable-html-components-how-to-reuse-a-header-and-footer-on-a-website/
