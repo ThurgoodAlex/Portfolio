b701be22440b6d6447e56bc402faf0b00216ac66
ThurgoodAlex <adthurgood@gmail.com>
2024-10-02 13:40:26 -0600

    fixed the typing effect

--- src/animations.js
+++ src/animations.js
@@ -1,23 +1,29 @@
 //https://developer.mozilla.org/en-US/docs/Web/API/setInterval
 function typeWriter(elementId, speed) {
-    const element = document.getElementById(elementId);
-    const text = element.innerHTML;
-    let i = 0;
-    element.innerHTML = ''; // Clear existing content
+    
+    return new Promise((resolve) => {
+        const element = document.getElementById(elementId);
+        const text = element.innerHTML; // Get the initial text
+        let i = 0;
+        element.innerHTML = ''; // Clear existing content
+        element.style.visibility = 'visible';
 
-    const typing = setInterval(() =>{
-        if (i < text.length){
-            element.innerHTML += text.charAt(i);
-            i++
-        }
-        else{
-            clearInterval(typing)
-        }
-
-    }, speed)
+        const typing = setInterval(() => {
+            if (i < text.length) {
+                const char = text.charAt(i);
+                element.innerHTML += char; // Append next character
+                i++;
+            } else {
+                clearInterval(typing); // Stop the typing effect
+                resolve(); // Resolve the Promise when done
+            }
+        }, speed);
+    });
 }
 
-  window.onload = function(){
-    typeWriter("name", 225);
 
-  };
+
+window.onload = async function() {
+    await typeWriter("first", 225);
+    await typeWriter("last", 225)
+};
